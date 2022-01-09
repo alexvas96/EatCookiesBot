562f37febf39c5830d4d2b02f9b3a85804f6ad1e
alexvas96 <alexvas1996@ya.ru>
2022-01-09 22:06:23 +0300

    add periodic tasks

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import asyncio
+from datetime import datetime
+
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.message import ContentTypes
 
@@ -8,10 +11,30 @@ from settings import API_TOKEN
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+chat_ids = set()
 
-@dp.message_handler(commands=['start', 'help'])
+
+@dp.message_handler(commands=['start'])
 async def send_welcome(msg: types.Message) -> None:
-    await msg.answer(f'Я бот. Приятно познакомиться, {msg.from_user.id}')
+    chat_ids.add(msg.chat.id)
+    await msg.answer(f'Я бот. Приятно познакомиться, @{msg.from_user.username}')
+
+
+async def create_lunch_poll(chat_id: str):
+    options = []
+
+    with session_scope() as session:
+        places = session.query(Place).all()
+        for p in places:
+            options.append(p.name)
+
+    await bot.send_poll(
+        chat_id=chat_id,
+        question='Откуда заказываем?',
+        options=options,
+        is_anonymous=False,
+        open_period=600,
+    )
 
 
 @dp.message_handler(content_types=ContentTypes.TEXT)
@@ -22,29 +45,23 @@ async def get_text_messages(msg: types.Message) -> None:
         await msg.answer('Привет!')
 
     elif 'обед' in msg_lower:
-        options = []
+        await create_lunch_poll(chat_id=msg.chat.id)
 
-        with session_scope() as s:
-            places = s.query(Place).all()
-            for p in places:
-                options.append(p.name)
 
-        msg_with_poll = await bot.send_poll(
-            chat_id=msg.chat.id,
-            question='Откуда заказываем?',
-            options=options,
-            is_anonymous=False,
-            open_period=600,
-        )
+async def periodic(sleep_for: int, hour: int, minute: int) -> None:
+    while True:
+        await asyncio.sleep(sleep_for)
+        now = datetime.now()
 
-        print(msg_with_poll.poll.values)
-
-    else:
-        await msg.answer('Не понимаю, что это значит.')
+        if now.hour == hour and now.minute == minute:
+            for id_ in chat_ids:
+                await create_lunch_poll(chat_id=id_)
 
 
 def main() -> None:
-    executor.start_polling(dp)
+    loop = asyncio.get_event_loop()
+    loop.create_task(periodic(60, hour=10, minute=30))
+    executor.start_polling(dp, loop=loop)
 
 
 if __name__ == '__main__':
